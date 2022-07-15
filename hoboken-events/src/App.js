fb7810c0655030e9364716e07254b27c0cb2cd18
Alex Saltstein <asaltste@gmail.com>
2022-07-14 22:20:33 -0400

    added event Item component

--- hoboken-events/src/App.js
+++ hoboken-events/src/App.js
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
+import { EventItem } from "./components/happy_hours/EventItem";
 
 export default function App() {
   return (
     <div>
-      <h1>Bookkeeper</h1>
+      <h1>Hoboken Happy hours</h1>
       <nav
         style={{
           borderBottom: "solid 1px",
           paddingBottom: "1rem",
         }}
       >
-        <Link to="/happyhour">Happy Hour</Link> |{" "}
+        {/* example link for use when we add events */}
+        <Link to="/happyhour">Happy Hour</Link>
+        <EventItem/>
       </nav>
     </div>
   );
---
+++ hoboken-events/src/components/happy_hours/EventItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const EventItem = () => {
+  return (
+    <div>Event item</div>
+  )
+}
