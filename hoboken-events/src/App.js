47b608aecdf357489a675effa7ded59494cca955
Alex Saltstein <asaltste@gmail.com>
2022-07-14 23:24:45 -0400

    test 2

--- hoboken-events/src/App.js
+++ hoboken-events/src/App.js
@@ -1,6 +1,7 @@
-import './index.css'
+import "./index.css";
 // import { Link } from "react-router-dom";
 import { EventList } from "./components/events/EventList";
+import { EventItem } from "./components/events/EventItem";
 
 export default function App() {
   return (
@@ -14,7 +15,7 @@ export default function App() {
       >
         {/* example link for use when we add events */}
         {/* <Link to="/happyhour">Happy Hour</Link> */}
-        <EventList title="Happy Hours" url="https://reqres.in/api/users"/>
+        <EventList title="Happy Hours" url="https://reqres.in/api/users" />
       </nav>
     </div>
   );
