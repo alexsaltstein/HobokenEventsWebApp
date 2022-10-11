1f50ef3fe1e7a7c144334fbb820584664f9f75f2
Shinyships <xxandynguyenxx@gmail.com>
2022-10-11 14:31:12 -0400

    fixed Icon problem and indentation

--- src/components/form/Autocomplete.jsx
+++ src/components/form/Autocomplete.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React from "react";
 import { SearchIcon } from '../icons';
-import {DebounceInput} from 'react-debounce-input';
+import { DebounceInput } from 'react-debounce-input';
 
 
 
@@ -23,25 +23,27 @@ export const Autocomplete = ({ input, setInput, setError, setGoogleData, placeIn
       },[input]);
 
     return (
-    <div className={`relative w-[95%] m-auto mb-2`}>
-      <label
-        htmlFor={"What's the name of place?"}
-        className="absolute left-1 top-1 z-10 m-1 text-input-label-gray text-sm"
-      >
-      What's the name of place?
-     <span className="text-red-400">*</span>
-      </label>
-      <div className="absolute right-2 top-4">
-        <svg className="w-6 h-6 relative float-right" fill="none" stroke="#A6A6A6" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          {SearchIcon}
-        </svg>
-      </div>
-            <DebounceInput
+      <>
+        <div className='relative w-[95%] m-auto mb-2'>
+          <label
+            htmlFor={"Name of Location"}
+            className="absolute left-1 top-1 z-10 m-1 text-input-label-gray text-sm"
+          >
+            Name of Location
+            <span className="text-red-400">&nbsp;*</span>
+          </label>
+          <div className="absolute right-2 top-4">
+            <svg className="w-6 h-6 relative float-right" fill="none" stroke="#A6A6A6" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              {<SearchIcon />}
+            </svg>
+          </div>
+          <DebounceInput
             className="border mt-1 mb-1 rounded w-full h-12 pl-2 pt-5"
             value={placeInfo.name}
             debounceTimeout={600}
             onChange={event => setInput(event.target.value)}
-            />
-      </div>
+          />
+        </div>
+      </>
     )
 }
