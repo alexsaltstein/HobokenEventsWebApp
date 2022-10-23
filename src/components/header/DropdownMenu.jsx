34a174187fef0af900f55fa6317e9e3b434f5176
Shinyships <xxandynguyenxx@gmail.com>
2022-10-23 16:20:52 -0400

    Responsiveness update

--- src/components/header/DropdownMenu.jsx
+++ src/components/header/DropdownMenu.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import { SubmitButton, AboutButton, ContactUsButton, SignInButton, SignOutButton } from '../icons/Icons';
 import './Header.css';
 
-export default function DropdownMenu({ effect }) {
-    const authed = true; //placeholder for real authorization check
+export default function DropdownMenu({ effect, authed }) {
     return (
         <>
-            <div className="relative text-left z-0">
-                <div className={`absolute transition-all duration-500 ${effect ? 'top-7' : '-top-40'} left-0 mt-5 w-full origin-top-left bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`} role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1" id='menu'>
+            <div className="relative text-left z-10 md:hidden">
+                <div className={`absolute transition-all duration-500 ${effect ? 'top-9' : '-top-60'} left-0 mt-5 w-full origin-top-left bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`} role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1" id='menu'>
                     <div className="py-1" role="none">
                         { !authed ? null :
                             <a href="#" className="text-gray-700 px-4 py-2 text-sm flex hover:bg-gray-100" role="menuitem" tabIndex="-1" id="menu-item-0">
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,41 +5,55 @@ import './Header.css';
 
 export default function Header() {
     const [effect, setEffect] = useState(false);
+    const authed = false; //placeholder for real authorization check
 
     function animateMenuIcon() {
-        const container = document.getElementById('menuButton');
-        container.classList.toggle("change");
         setEffect(!effect);
     }
 
     return (
         <>
+            <nav class="absolute flex top-0 left-0 h-14 w-screen bg-hoboken-blue border-gray-200 py-2.5 z-20">
+                <div class="absolute top-0 left-0 container flex flex-wrap justify-between items-center mx-auto md:absolute md:top-auto">
+                    <a href="/" class="flex items-center">
+                        {/* INSERT LOGO HERE */}
+                        <span class="ml-2 my-auto self-center text-xl font-semibold whitespace-nowrap text-white">Hudson Happenings</span>
+                    </a>
+                    <button type="button" class="inline-flex items-center p-2 ml-3 mr-3 mt-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false" onClick={animateMenuIcon} onAnimationEnd={() => setEffect(false)}>
+                        <svg class="w-6 h-6" aria-hidden="true" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                    </button>
+                    <div class="hidden w-full md:block md:w-auto md:fixed md:right-0" id="navbar-default">
+                        <ul class="flex flex-col ml-auto p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-hoboken-blue">
+                            <li>
+                                <a href="#" class="block py-2 pr-4 pl-3 text-white hover:text-gray-200 md:border-0 md:p-0">Home</a>
+                            </li>
+                            <li>
+                                <a href="#" class="block py-2 pr-4 pl-3 text-white hover:text-gray-200 md:border-0 md:p-0">About</a>
+                            </li>
+                            {
+                                !authed ? null :
+                                <li>
+                                <a href="#" class="block py-2 pr-4 pl-3 text-white hover:text-gray-200 md:border-0 md:p-0">Submit a Happening</a>
+                                </li>
+                            }
+                            <li>
+                                <a href="#" class="block py-2 pr-4 pl-3 text-white hover:text-gray-200 md:border-0 md:p-0">Contact Us</a>
+                            </li>
+                            <li>
+                                <a href="/admin/login" class="block py-2 pr-4 pl-3 text-white hover:text-gray-200 md:border-0 md:p-0">
+                                    <p>
+                                        {!authed ? 'Sign In' : 'Sign Out'}
+                                    </p>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
             <div className="relative w-screen md:w-1/2">
-                <DropdownMenu effect={effect}/>
-            </div>
-            <div className='absolute flex top-0 left-0 w-screen h-12 bg-hoboken-blue z-10' id='header'>
-                <div className="relative left-2 top-[18px] w-1/6 h-2 z-10 md:w-[2%]" id="menuButton" onClick={animateMenuIcon} onAnimationEnd={() => setEffect(false)}>
-                    <div className="bar1"></div>
-                    <div className="bar2"></div>
-                    <div className="bar3"></div>
-                </div>
-                <div className="w-2/3 md:w-[70%] lg:w-10/12 xl:w-[90%] 2xl:w-[95%] text-center md:text-left">
-                    <img src='logo.png' alt='logo' className='hidden relative top-0 left-2 right-0 h-full aspect-[3/1]'/>
-                    <h1 className='text-white font-bold m-2 pl-1 text-xl'>Hoboken Events</h1>
-                </div>
-                <div className="w-1/6 md:w-[30%] lg:w-2/12 xl:w-[10%] grid grid-cols-2 right-2 justify-items-end text-white text-xs">
-                    <div className='hidden md:inline-block hover:bg-[#788A9B] mr-2 w-full'>
-                        <div className='relative top-2 left-0 right-0 w-full text-center text-lg'>
-                            Login
-                        </div>
-                    </div>
-                    <div className='hidden md:inline-block hover:bg-[#788A9B] ml-2 w-full'>
-                        <div className='relative top-2 left-0 right-0 w-full text-center text-lg'>
-                            Sign Up
-                        </div>
-                    </div>
-                </div>
+                <DropdownMenu effect={effect} authed={authed}/>
             </div>
         </>
+        
     );
 }
