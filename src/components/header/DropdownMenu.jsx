1ede965eaed849b83cc398221dd139dac122eed3
Shinyships <xxandynguyenxx@gmail.com>
2022-10-23 17:34:36 -0400

    Button, ordering, and title changes

--- src/components/header/DropdownMenu.jsx
+++ src/components/header/DropdownMenu.jsx
@@ -2,29 +2,29 @@ import React from 'react';
 import { SubmitButton, AboutButton, ContactUsButton, SignInButton, SignOutButton } from '../icons/Icons';
 import './Header.css';
 
-export default function DropdownMenu({ effect, authed }) {
+export default function DropdownMenu({ open, authed }) {
     return (
         <>
             <div className="relative text-left z-10 md:hidden">
-                <div className={`absolute transition-all duration-500 ${effect ? 'top-9' : '-top-60'} left-0 mt-5 w-full origin-top-left bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`} role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1" id='menu'>
+                <div className={`absolute transition-all duration-500 ${open ? 'top-9' : '-top-60'} left-0 mt-5 w-full origin-top-left bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`} role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1" id='menu'>
                     <div className="py-1" role="none">
                         { !authed ? null :
                             <a href="#" className="text-gray-700 px-4 py-2 text-sm flex hover:bg-gray-100" role="menuitem" tabIndex="-1" id="menu-item-0">
                                 <SubmitButton params={'mr-2'}/>
-                                <p>
-                                    Submit a Deal
+                                <p className='mt-auto mb-auto'>
+                                    What's Happening?
                                 </p>
                             </a>
                         }
                         <a href="#" className="text-gray-700 px-4 py-2 text-sm flex hover:bg-gray-100" role="menuitem" tabIndex="-1" id="menu-item-1">
                             <AboutButton params={'mr-2'}/>
-                            <p>
+                            <p className='mt-auto mb-auto'>
                                 About
                             </p>
                         </a>
                         <a href="/admin/login" className="text-gray-700 px-4 py-2 text-sm flex hover:bg-gray-100" role="menuitem" tabIndex="-1" id="menu-item-2">
                             <ContactUsButton params={'mr-2'}/>
-                            <p>
+                            <p className='mt-auto mb-auto'>
                                 Contact Us
                             </p>
                         </a>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,57 +1,64 @@
 import React, {useState} from 'react';
 import DropdownMenu from './DropdownMenu';
+import { SubmitButton } from '../icons/Icons';
 import './Header.css';
 
 
 export default function Header() {
-    const [effect, setEffect] = useState(false);
-    const authed = false; //placeholder for real authorization check
+    const [open, setOpen] = useState(false);
+    const authed = true; //placeholder for real authorization check
 
-    function animateMenuIcon() {
-        setEffect(!effect);
+    function openMenu() {
+        setOpen(!open);
     }
 
     return (
         <>
-            <nav class="absolute flex top-0 left-0 h-14 w-screen bg-hoboken-blue border-gray-200 py-2.5 z-20">
-                <div class="absolute top-0 left-0 container flex flex-wrap justify-between items-center mx-auto md:absolute md:top-auto">
-                    <a href="/" class="flex items-center">
+            <nav className="absolute flex top-0 left-0 h-14 w-screen bg-hoboken-blue border-gray-200 py-2.5 z-20">
+                <div className="absolute top-0 left-0 container flex flex-wrap justify-between items-center mx-auto md:absolute md:top-auto">
+                    <a href="/" className="flex items-center">
                         {/* INSERT LOGO HERE */}
-                        <span class="ml-2 my-auto self-center text-xl font-semibold whitespace-nowrap text-white">Hudson Happenings</span>
+                        <span className="ml-2 my-auto self-center text-xl font-semibold whitespace-nowrap text-white">Hudson Happs</span>
                     </a>
-                    <button type="button" class="inline-flex items-center p-2 ml-3 mr-3 mt-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false" onClick={animateMenuIcon} onAnimationEnd={() => setEffect(false)}>
-                        <svg class="w-6 h-6" aria-hidden="true" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                    <button type="button" className="inline-flex items-center p-2 ml-3 mr-3 mt-2 text-sm outline-none text-gray-500 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false" onClick={openMenu} onAnimationEnd={() => setOpen(false)}>
+                        {
+                            open ? 
+                            <svg className="w-6 h-6" fill="none" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg> :
+                            <svg className="w-6 h-6" aria-hidden="true" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                        }
                     </button>
-                    <div class="hidden w-full md:block md:w-auto md:fixed md:right-0" id="navbar-default">
-                        <ul class="flex flex-col ml-auto p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-hoboken-blue">
-                            <li>
-                                <a href="#" class="block py-2 pr-4 pl-3 text-white hover:text-gray-200 md:border-0 md:p-0">Home</a>
+                    <div className="hidden w-full md:block md:w-auto md:fixed md:right-10" id="navbar-default">
+                        <ul className="flex flex-col ml-auto mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-hoboken-blue">
+                            <li key="about">
+                                <a href="#" className={`block py-2 pr-4 pl-3 mt-${authed ? 4 : 2} text-white hover:text-gray-200 md:border-0 md:p-0`}>About</a>
                             </li>
-                            <li>
-                                <a href="#" class="block py-2 pr-4 pl-3 text-white hover:text-gray-200 md:border-0 md:p-0">About</a>
+                            <li key="contact">
+                                <a href="#" className={`block py-2 pr-4 pl-3 mt-${authed ? 4 : 2} text-white hover:text-gray-200 md:border-0 md:p-0`}>Contact Us</a>
                             </li>
-                            {
-                                !authed ? null :
-                                <li>
-                                <a href="#" class="block py-2 pr-4 pl-3 text-white hover:text-gray-200 md:border-0 md:p-0">Submit a Happening</a>
-                                </li>
-                            }
-                            <li>
-                                <a href="#" class="block py-2 pr-4 pl-3 text-white hover:text-gray-200 md:border-0 md:p-0">Contact Us</a>
-                            </li>
-                            <li>
-                                <a href="/admin/login" class="block py-2 pr-4 pl-3 text-white hover:text-gray-200 md:border-0 md:p-0">
+                            <li >
+                                <a href="/admin/login" className={`block py-2 pr-4 pl-3 mt-${authed ? 4 : 2} text-white hover:text-gray-200 md:border-0 md:p-0`}>
                                     <p>
                                         {!authed ? 'Sign In' : 'Sign Out'}
                                     </p>
                                 </a>
                             </li>
+                            {
+                                !authed ? null :
+                                <button
+                                    className="flex py-2 pr-4 pl-3 bg-button-blue border-gray-200 text-white mt-2 mr-3 text-sm font-semibold rounded-lg"
+                                >
+                                    <SubmitButton params={'mr-2'}/>
+                                    <p className='m-auto'>
+                                        What's Happening?
+                                    </p>
+                                </button>
+                            }
                         </ul>
                     </div>
                 </div>
             </nav>
             <div className="relative w-screen md:w-1/2">
-                <DropdownMenu effect={effect} authed={authed}/>
+                <DropdownMenu open={open} authed={authed}/>
             </div>
         </>
         
