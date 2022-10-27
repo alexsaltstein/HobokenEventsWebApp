f8e50f82c6895789abf0a842822cf3c14c131c03
Shinyships <xxandynguyenxx@gmail.com>
2022-10-27 17:44:57 -0400

    banner min height

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -4,10 +4,10 @@ export default function Banner() {
     return (
         <>
             <div className="relative w-screen aspect-auto">
-                <div className="relative top-14 left-0 flex w-full h-full">
+                <div className="relative top-14 left-0 flex w-full h-full max-h-96">
                     <div className="overflow-hidden">
                         <img className="xl:hidden" src="/hudson.webp" alt="banner"/>
-                        <img className="hidden xl:block" src="/skyline4k.jpg" alt="4kbanner"/>
+                        <img className="hidden xl:block w-full h-full object-cover object-center" src="/nyclarge.jpg" alt="4kbanner"/>
                         <div className="absolute top-0 w-full h-full opacity-50 bg-gradient-to-r from-gray-500 to-transparent" />
                     </div>
                     <div className="absolute flex h-1/3 items-center">
