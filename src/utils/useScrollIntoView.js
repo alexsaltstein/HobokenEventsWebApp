478d3a905b8ca70d10e22c2d9e5d2de650ce1ccd
Alex Saltstein <asaltste@gmail.com>
2022-11-27 10:32:04 -0500

    scroll into view hook

--- src/utils/useScrollIntoView.js
+++ src/utils/useScrollIntoView.js
@@ -4,7 +4,6 @@ export const useScrollIntoView = (deps) => {
   const ref = React.useRef(null);
   React.useEffect(() => {
     if (ref.current) {
-      console.log("here");
       ref.current.scrollIntoView({ behavior: "smooth" });
     }
   }, deps);
