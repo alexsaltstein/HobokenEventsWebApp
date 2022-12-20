87d508eb2db99068c45dc458b0522be028d9dfdc
Shinyships <xxandynguyenxx@gmail.com>
2022-12-19 22:37:52 -0500

    pinned report to bottom right instead of floating right

--- src/components/events/EventItem.js
+++ src/components/events/EventItem.js
@@ -66,7 +66,7 @@ export const EventItem = ({ eventData, onClickReport, setReportedDeal, setReport
             
           </div>
           </Link>
-          <div className="hidden float-right mb-4"> {/* should be flex when reporting is implemented */}
+          <div className="fixed flex bottom-0 right-4 mb-4"> {/* should be flex when reporting is implemented */}
               <svg class="w-6 h-6 text-red-400 mr-1 pt-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path></svg>
               <button className="text-red-400 z-20"
                 onClick={() => {
