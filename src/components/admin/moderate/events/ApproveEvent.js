c2f42a4823aca101b2a03dd5a24b8708d5c247ef
Shinyships <xxandynguyenxx@gmail.com>
2023-01-02 10:30:09 -0500

    Added Official icons

--- src/components/admin/moderate/events/ApproveEvent.js
+++ src/components/admin/moderate/events/ApproveEvent.js
@@ -1,6 +1,8 @@
+import "twin.macro"
 import axios from "axios";
 import React from "react";
 import { useUserState } from "../../../../utils/userState";
+import { CheckIcon } from "../../../icons/Icons";
 
 export const ApproveEvent = ({ eventId }) => {
   const [user] = useUserState();
@@ -24,8 +26,9 @@ export const ApproveEvent = ({ eventId }) => {
   return (
     <button
       onClick={onClick}
-      className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 mt-4">
-        Approve ✓
+      className="flex focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 mt-4">
+        Approve
+        <CheckIcon tw="ml-2 h-6 w-6" />
     </button>
   );
 };
