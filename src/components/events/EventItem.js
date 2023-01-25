1e53751d9536405bd8d01259395939fc82a84b64
Alex Saltstein <asaltste@gmail.com>
2023-01-24 22:22:03 -0500

    fix report on safari

--- src/components/events/EventItem.js
+++ src/components/events/EventItem.js
@@ -30,12 +30,12 @@ export const EventItem = ({ eventData, moderate }) => {
     deals,
     place,
     updatedAt,
-    _id
+    _id,
   } = eventData;
-  
-    const reportData = {
+
+  const reportData = {
     dealId: _id,
-  }
+  };
 
   React.useEffect(() => {
     if (showReportModal) {
@@ -54,7 +54,7 @@ export const EventItem = ({ eventData, moderate }) => {
           reportData={reportData}
           onDismiss={() => setShowReportModal(false)}
         />
-      ) : null} 
+      ) : null}
       <div className="font-sans mx-4 mb-4 overflow-y-hidden drop-shadow-md transition duration-200 hover:shadow-lg">
         <div className="bg-white border p-4 h-full z-30">
           <Link to={`/place/${placeId}`}>
@@ -84,69 +84,75 @@ export const EventItem = ({ eventData, moderate }) => {
                   );
                 })}
               </div>
-            <div className="flex">
-              <TimerIcon tw="mr-2 h-6 text-gray-500" />
-              <p className="mb-2">{getDisplayTime(startTime, endTime)}</p>
-            </div>
-            <hr />
-            <div className="md:whitespace-normal mt-2 mb-8" id="description">
-              {deals.map((deal, index) =>
-                deal.includes("https") ? (
-                  <div className="flex" key={`${deal._id}-${deal}-${index}-wrapper`}>
-                    <ExternalLinkIcon tw="mt-1 mr-2 text-gray-500 h-6" key={`${deal._id}-${deal}-${index}-link-icon`}/>
-                    <button
-
+              <div className="flex">
+                <TimerIcon tw="mr-2 h-6 text-gray-500" />
+                <p className="mb-2">{getDisplayTime(startTime, endTime)}</p>
+              </div>
+              <hr />
+              <div className="md:whitespace-normal mt-2 mb-8" id="description">
+                {deals.map((deal, index) =>
+                  deal.includes("https") ? (
+                    <div
+                      className="flex"
+                      key={`${deal._id}-${deal}-${index}-wrapper`}
+                    >
+                      <ExternalLinkIcon
+                        tw="mt-1 mr-2 text-gray-500 h-6"
+                        key={`${deal._id}-${deal}-${index}-link-icon`}
+                      />
+                      <button key={`${deal._id}-${deal}-${index}`}>
+                        <a
+                          href={deal}
+                          target="_blank"
+                          rel="noreferrer"
+                          key={`${deal._id}-${deal}-${index}-link`}
+                        >
+                          View Deal Menu
+                        </a>
+                      </button>
+                    </div>
+                  ) : (
+                    <p
+                      className="text-gray-700"
                       key={`${deal._id}-${deal}-${index}`}
                     >
-                      <a href={deal} target="_blank" rel="noreferrer" key={`${deal._id}-${deal}-${index}-link`}>
-                        View Deal Menu
-                      </a>
-                    </button>
-                  </div>
-                ) : (
-                  <p
-                    className="text-gray-700"
-                    key={`${deal._id}-${deal}-${index}`}
-                  >
-                    ∙{deal}
-                  </p>
-                )
-              )}
+                      ∙{deal}
+                    </p>
+                  )
+                )}
+              </div>
             </div>
-          </div>
-        </Link>
-        <div className="flex flex-wrap w-full">
-          <div className="flex items-center space-x-1">
-            <CheckIcon className="text-green-600" />
-            <p className="text-gray-500">
-              Verified: {displayDate(new Date(updatedAt))}
-            </p>
-          </div>
-          { moderate ?
-            null :
-            <div className="fixed flex bottom-0 right-4 mb-[18px] fol:max-xs:items-center fold:max-xs:space-x-1 fold:max-xs:relative fold:max-xs:right-0 fold:max-xs:mt-2">
-              <CautionIcon tw="text-red-400 mr-1 pt-1 h-6" />
-              <button
-                className="text-gray-500 z-20"
-                onClick={() => {
-                  setShowReportModal(true);
-                }}
-              >
-                Report Deal
-              </button>
+          </Link>
+          <div className="flex justify-between items-center">
+            <div className="flex items-center space-x-1">
+              <CheckIcon className="text-green-600" />
+              <p className="text-gray-500 lg:text-sm">
+                Verified: {displayDate(new Date(updatedAt))}
+              </p>
             </div>
-          }
-        </div>
-          { moderate ?
+            {moderate ? null : (
+              <div className="flex items-center">
+                <CautionIcon tw="text-red-400 mr-1 h-4" />
+                <button
+                  className="text-gray-500 z-20 lg:text-sm"
+                  onClick={() => {
+                    setShowReportModal(true);
+                  }}
+                >
+                  Report Deal
+                </button>
+              </div>
+            )}
+          </div>
+          {moderate ? (
             <div className="flex flex-row space-x-4 justify-center items-center border-t">
               <p className="text-input-label-gray">Actions:</p>
               <ApproveEvent eventId={eventData._id} />
               <DenyEvent eventId={eventData._id} />
-            </div> :
-            null
-          }
-          </div>
-       </div>
+            </div>
+          ) : null}
+        </div>
+      </div>
     </>
   );
 };
