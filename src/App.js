7992984f04859837c6ee0acb51f92bae266afcb8
Shinyships <xxandynguyenxx@gmail.com>
2023-02-02 19:03:57 -0500

    Added banner ads back and set up space to put map in future

--- src/App.js
+++ src/App.js
@@ -47,38 +47,51 @@ export default function App() {
 
   return (
     <div className="overflow-y-hidden overflow-x-hidden">
-      <GoogleTags />
-      <Banner />
-      <EventList
-        url={`${
-          process.env.REACT_APP_API_URL
-        }/api/deal?approved.state=active&dayOfWeek=${getDayOfWeek(
-          selectedDate.getDay()
-        )}&${filterResult}`}
-        setNumResults={setNumResults}
+        <GoogleTags />
+        <Banner />
+        <div className="flex">
+          <div>
+            <EventList
+              url={`${
+                process.env.REACT_APP_API_URL
+              }/api/deal?approved.state=active&dayOfWeek=${getDayOfWeek(
+                selectedDate.getDay()
+              )}&${filterResult}`}
+              setNumResults={setNumResults}
 
-        menu={<FilterMenuDesktop filters={filters} setFilters={setFilters}/>}
+              menu={<FilterMenuDesktop filters={filters} setFilters={setFilters}/>}
 
-        calendar={
-          <Calendar
-            selectedDate={selectedDate}
-            onDateChange={onDateChange}
-            filterButton={
-              <button
-                className="relative h-6 w-6 top-14 right-8 md:top-5 lg:hidden"
-                onClick={() => setOpen(true)}
-              >
-                <FilterIcon fill={isOpen ? "#007bc7" : "none"} stroke={isOpen ? "none" : "#6f7287"} tw={"w-7 h-7"}/>
-              </button>
-            }
-          >
-            <FilterBottomSheet isOpen={isOpen} setOpen={setOpen} filters={filters} setFilters={setFilters} numResults={numResults}/>
-          </Calendar>
-        }
-      />
-      <div className="lg:hidden max-w-full max-h-fit mt-4 mx-4">
-        <BannerAd />
+              calendar={
+                <Calendar
+                  selectedDate={selectedDate}
+                  onDateChange={onDateChange}
+                  filterButton={
+                    <button
+                      className="relative h-6 w-6 top-14 right-8 md:top-5 lg:hidden"
+                      onClick={() => setOpen(true)}
+                    >
+                      <FilterIcon fill={isOpen ? "#007bc7" : "none"} stroke={isOpen ? "none" : "#6f7287"} tw={"w-7 h-7"}/>
+                    </button>
+                  }
+                >
+                  <FilterBottomSheet isOpen={isOpen} setOpen={setOpen} filters={filters} setFilters={setFilters} numResults={numResults}/>
+                </Calendar>
+              }
+            />
+            <div className="flex max-w-full max-h-fit mt-4 mx-4">
+              <div className="h-fit w-full m-4">
+                <BannerAd />
+              </div>
+              <div className="h-fit w-full m-4">
+                <BannerAd />
+              </div>
+            </div>
+          </div>
+        <div className="hidden border border-red-500">
+              {/* PUT MAP HERE */}
+        </div>
       </div>
+      
     </div>
   );
 }
