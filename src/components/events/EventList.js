57cd50ac2924119adf8d501cbc5b93e6fc042d7a
benevolent17 <johndyercba@gmail.com>
2023-02-03 20:00:51 -0500

    implement inbetween ads for mobile

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { EventItem } from "./EventItem";
 import { Loading } from "../../utils/Loading";
 import { ResponsiveGridHP } from "../templates/ResponsiveGrid";
 import { BannerAd } from "../ads/BannerAd";
 import { VerticalBannerAd } from "../ads/VerticalBannerAd";
+
 export const EventList = ({ url }) => {
   const [loading, setLoading] = React.useState(true);
   const [eventData, setEventData] = React.useState(null);
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 1450);
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 1450);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
 
   const fetchData = React.useCallback(async () => {
     try {
@@ -50,11 +61,16 @@ export const EventList = ({ url }) => {
           <VerticalBannerAd />
         </div>
         <div className="relative md:columns-2 w-full md:gap-0 xl:columns-3">
-        {eventData.map((event, index) => (
-          <div className="mx-4 mb-4 overflow-y-hidden">
-          <EventItem key={`list-item-${index}`} eventData={event} />
-          </div>
-        ))}
+          {eventData.map((event, index) => (
+            <div>
+              <EventItem key={`list-item-${index}`} eventData={event} />
+              {index > 0 && index % 3 === 0 && !isDesktop ? 
+                <div className="lg:hidden max-w-full max-h-fit mt-4 mx-4">
+                  <BannerAd />
+                </div>
+              : ''}
+            </div>
+          ))}
         </div>
         <div className="hidden lg:flex border-gray-100 border-2 max-w-full max-h-full">
           <VerticalBannerAd />
