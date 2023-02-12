1d8b7090cafba9e552e4f3f28aa041cc4c501be7
Alex Saltstein <asaltste@gmail.com>
2023-02-12 15:28:14 -0500

    added new marker and css

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,13 +1,8 @@
 import React from "react";
-import {
-  GoogleMap,
-  useJsApiLoader,
-  Marker,
-  InfoWindow,
-} from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, OverlayView } from "@react-google-maps/api";
 import axios from "axios";
 import { LoadingAnimation } from "../icons/LoadingAnimation";
-
+import "./test.css";
 const containerStyle = {
   width: "100%",
   height: "100%",
@@ -19,12 +14,18 @@ const center = {
   lng: -74.034775,
 };
 
+const getPixelPositionOffset = (width, height) => ({
+  x: -(width / 2),
+  y: -(height / 2),
+});
+
 const Map = ({ deals }) => {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   });
 
+  const [selectedPlace, setSelectedPlace] = React.useState(null);
   const [map, setMap] = React.useState(null);
   const [markerLocations, setMarkerLocations] = React.useState([]);
 
@@ -60,20 +61,46 @@ const Map = ({ deals }) => {
       onLoad={onLoad}
       onUnmount={onUnmount}
     >
-      {markerLocations.map((mark) => (
-        <Marker
-          key={mark.dealId}
-          title={mark.title}
-          label={mark.placeName}
-          position={{ lat: mark.lat, lng: mark.lng }}
-          onClick={() => {
-            map.panTo({ lat: mark.lat, lng: mark.lng });
-            if (map.zoom !== 17) {
-              map.setZoom(17);
-            }
-          }}
-        ></Marker>
-      ))}
+      {markerLocations.map((mark, index) => {
+        const selected = selectedPlace === index;
+        return (
+          <OverlayView
+            position={{ lat: mark.lat, lng: mark.lng }}
+            mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+            getPixelPositionOffset={getPixelPositionOffset}
+          >
+            <button
+              onClick={() => {
+                setSelectedPlace(index);
+                map.panTo({ lat: mark.lat, lng: mark.lng });
+                if (map.zoom !== 17) {
+                  map.setZoom(17);
+                }
+              }}
+              title={mark.title}
+            >
+              <div className="popup-container">
+                <div
+                  className={`popup-bubble ${
+                    selected ? "bg-green-600" : "bg-button-blue"
+                  }`}
+                >
+                  <h1 className={`text-white ${selected ? "font-bold" : null}`}>
+                    {mark.placeName}
+                  </h1>
+                </div>
+                <div
+                  className={`${
+                    selected
+                      ? "popup-bubble-anchor-selected"
+                      : "popup-bubble-anchor"
+                  }`}
+                />
+              </div>
+            </button>
+          </OverlayView>
+        );
+      })}
     </GoogleMap>
   ) : (
     <div className="flex items-center justify-center w-full h-full">
