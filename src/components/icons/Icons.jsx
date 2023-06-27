37253bfe21b028038b5f9953bce20f098708e948
Alex Saltstein <asaltste@gmail.com>
2023-06-27 19:47:18 -0400

    merge

--- src/components/icons/Icons.jsx
+++ src/components/icons/Icons.jsx
@@ -643,3 +643,21 @@ export const MailIcon = (props) => (
     />
   </svg>
 );
+
+export const SortIcon = (props) => (
+  <svg
+    aria-hidden="true"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={1.5}
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
