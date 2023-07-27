7ddee4815ab9b70d54bad14599840c356692248a
Alex Saltstein <asaltste@gmail.com>
2023-07-27 19:55:02 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import ReactDOM from "react-dom/client";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
@@ -16,56 +15,65 @@ import { PowerHour } from "./routes/PowerHour";
 import { NotFound } from "./routes/NotFound";
 import GlobalStyles from "./styles/GlobalStyles";
 import { Toaster } from "react-hot-toast";
+import { hydrate, render } from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Toaster />
-    <GlobalStyles />
-    <RecoilRoot>
-      <BrowserRouter>
-        <div>
-          <Header />
-          <div className="min-h-[100vh] my-14 max-w-full" id="pageContent">
-            <Routes>
-              <Route path="/admin/login" element={<Login />} />
-              <Route
-                path="/admin/moderate/events"
-                element={
-                  <AuthedRoute>
-                    <ListEventsPage />
-                  </AuthedRoute>
-                }
-              />
-              <Route
-                path="/admin/create/events"
-                element={
-                  <AuthedRoute>
-                    <AddEventPage />
-                  </AuthedRoute>
-                }
-              />
-              <Route
-                path="/admin/logout"
-                element={
-                  <AuthedRoute>
-                    <Logout />
-                  </AuthedRoute>
-                }
-              />
-              <Route path="/place/:id" element={<Place />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/powerhour" element={<PowerHour />} />
-              {/* <Route path="/contact" element={<Contact />} /> */}
-              <Route path="/" element={<App />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
+const Index = () => {
+  return (
+    <React.StrictMode>
+      <Toaster />
+      <GlobalStyles />
+      <RecoilRoot>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <div className="min-h-[100vh] my-14 max-w-full" id="pageContent">
+              <Routes>
+                <Route path="/admin/login" element={<Login />} />
+                <Route
+                  path="/admin/moderate/events"
+                  element={
+                    <AuthedRoute>
+                      <ListEventsPage />
+                    </AuthedRoute>
+                  }
+                />
+                <Route
+                  path="/admin/create/events"
+                  element={
+                    <AuthedRoute>
+                      <AddEventPage />
+                    </AuthedRoute>
+                  }
+                />
+                <Route
+                  path="/admin/logout"
+                  element={
+                    <AuthedRoute>
+                      <Logout />
+                    </AuthedRoute>
+                  }
+                />
+                <Route path="/place/:id" element={<Place />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/powerhour" element={<PowerHour />} />
+                {/* <Route path="/contact" element={<Contact />} /> */}
+                <Route path="/" element={<App />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+            <div className="flex" id="footer">
+              <Footer />
+            </div>
           </div>
-          <div className="flex" id="footer">
-            <Footer />
-          </div>
-        </div>
-      </BrowserRouter>
-    </RecoilRoot>
-  </React.StrictMode>
-);
+        </BrowserRouter>
+      </RecoilRoot>
+    </React.StrictMode>
+  );
+};
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<Index />, rootElement);
+} else {
+  render(<Index />, rootElement);
+}
